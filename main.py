54a2f51ad491c2642e1762881d2c29cde3cf6970
Linxiangyu Yao <48883028+ylxy123@users.noreply.github.com>
2020-03-03 14:11:01 +0800

    add

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 import re
 from time import sleep
+import time
 import threading
 from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
@@ -24,9 +25,12 @@ from show_img import show_qrimg
 from mainWin import Ui_JD
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLineEdit, QMessageBox, QDialog
 import sys
-from PyQt5 import Qt
+import os
+import datetime
+from PyQt5 import Qt, QtGui
 from PyQt5.QtCore import QDateTime, QTimer
 from PyQt5 import QtCore
+from PyQt5.Qt import QThread
 from scanQR import Ui_Dialog
 
 log_url = 'https://passport.jd.com/uc/login'
@@ -73,16 +77,34 @@ def log_init():
 
 
 
+class EmittingStr(QtCore.QObject):
+    textWritten = QtCore.pyqtSignal(str)  # 定义一个发送str的信号
 
+    def write(self, text):
+        self.textWritten.emit(str(text))
 
+class ScanQrcode(QThread, QtCore.QObject):
+    sendtext = QtCore.pyqtSignal(bool)
+    def __init__(self):
+        super().__init__()
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+
+        self.sendtext.emit()
 
 
 # 界面类
 class JD_ui(QWidget,Ui_JD):
     def __init__(self):
+        sys.stdout = EmittingStr(textWritten=self.outputWritten)
+        sys.stderr = EmittingStr(textWritten=self.outputWritten)
         super(QWidget, self).__init__()
         self.setupUi(self)
         self.connecter()
+
 
         # 显示时间
         timer = QTimer(self)
@@ -97,11 +119,11 @@ class JD_ui(QWidget,Ui_JD):
 
     def connecter(self):
         self.LoginBtn.clicked.connect(self.QRlogin)
-        
+
 
     def QRlogin(self):
         # 日志模块初始化
-        logger = log_init()
+        self.logger = log_init()
 
         # 接管已打开浏览器
         # chrome_options = Options()
@@ -109,14 +131,14 @@ class JD_ui(QWidget,Ui_JD):
         # chrome_driver = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
         # driver = webdriver.Chrome(chrome_driver, chrome_options=chrome_options)
 
-        logger.info('FireFox内核运行中...')
+        self.outputWritten('FireFox内核运行中...')
 
         # 无头浏览器模式
-        driver = headless_firefox(self.log_url)
-        logger.info('京东-欢迎登录')
+        driver = headless_firefox('https://passport.jd.com/uc/login')
+        self.logger.info('京东-欢迎登录')
         self.log_url = driver.current_url
         img = driver.find_element_by_class_name('qrcode-img')
-        logger.info('二维码跳转中...')
+        self.logger.info('二维码跳转中...')
 
         # 保存二维码
         path = './qr/show.png'
@@ -125,13 +147,14 @@ class JD_ui(QWidget,Ui_JD):
         QRcode.save(path)
 
         # 打开保存的二维码
-
-        logger.info('请扫描二维码')
+        os.system('start ./qr/show.png')
+        self.logger.info('请扫描二维码')
         self.cur_url = driver.current_url
         while self.cur_url == self.log_url:
-            logger.warn('请注意：二维码未扫描！请扫二维码登录')
+            sleep(3)
+            self.logger.warn('请注意：二维码未扫描！请扫二维码登录')
             self.cur_url = driver.current_url
-        logger.info('登录成功！！！')
+        self.logger.info('登录成功！！！')
 
         # 保存登录cookies
         cookies = driver.get_cookies()
@@ -142,10 +165,28 @@ class JD_ui(QWidget,Ui_JD):
             f.write(jsonCookies)
         driver.quit()
 
+    def showlogtext(self, text):
+        self.logger.info(text)
+        print(f'>>> {datetime.datetime.now()} ' + '- ' + text + '\n')
+        self.log_terminal.append(f'{time.ctime()} > ' + text + '\n')
+        self.cursor = self.textBrowser_2.textCursor()
+        self.log_terminal.moveCursor(self.cursor.End)
+
+    def outputWritten(self, text):
+        cursor = self.log_terminal.textCursor()
+        cursor.movePosition(QtGui.QTextCursor.End)
+        cursor.insertText(text)
+        self.log_terminal.setTextCursor(cursor)
+        self.log_terminal.ensureCursorVisible()
+
+
+
+
 
 
 
 if __name__ == '__main__':
+
     app = QApplication(sys.argv)
     jd = JD_ui()
     jd.show()
